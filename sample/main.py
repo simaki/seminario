21b6c766abde5c2407c485c965bbfb88ac671f7b
Shota Imaki <simaki@SIs-Mac-mini.local>
2020-02-03 17:05:50 +0900

    mv seminar/ sample/

--- sample/main.py
+++ sample/main.py
@@ -4,13 +4,15 @@ import seminario
 CSV_DATABASE = './data/database.csv'
 SEMINAR_NAME = 'Seminar'
 DIR_ABSTRACT = './data/abstract/'
+CSS = 'data/poster/css/poster.css'
 
 
 def main():
-    seminario.interactive.main(
+    seminario.main(
         csv_database=CSV_DATABASE,
         seminar_name=SEMINAR_NAME,
         dir_abstract=DIR_ABSTRACT,
+        css=CSS,
     )
 
 
