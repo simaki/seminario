ca3e22a5df28d2d9807d13212129d07c9b29ac6c
Shota Imaki <simaki@SIs-Mac-mini.local>
2020-02-03 18:54:02 +0900

    add tests

--- tests/test_poster.py
+++ tests/test_poster.py
@@ -1,20 +1,25 @@
 import pytest
 
+from os.path import dirname
+from pathlib import Path
 import yaml
 
 from seminario import Seminar, PosterMaker
 from seminario.config import config
 
+tests_path = Path(dirname(__file__))
+
 
 params_path = [
-    'cases/valid/01.yml',
-    'cases/valid/02.yml',
-    'cases/valid/03.yml',
+    tests_path / 'cases/valid/01.yml',
+    tests_path / 'cases/valid/02.yml',
+    tests_path / 'cases/valid/03.yml',
 ]
 
 
 @pytest.mark.parametrize('path', params_path)
 def test_poster(path):
+    config.path.css = tests_path / 'data/poster/css/poster.css'
     with open(path) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
         seminar = Seminar(**data)
